1b5a01d82ba2d284ff6c5e10175b33c1f0c65d24
黄佳明 <ezrealok@163.com>
2020-12-10 11:20:36 +0800

    feat: 414 commit

---
+++ javabase/src/main/java/com/ezreal/leetcode/Simple414.java
@@ -0,0 +1,54 @@
+package com.ezreal.leetcode;
+
+import java.util.*;
+
+/**
+ * 414. 第三大的数
+ * 给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3, 2, 1]
+ * <p>
+ * 输出: 1
+ * <p>
+ * 解释: 第三大的数是 1.
+ * 示例 2:
+ * <p>
+ * 输入: [1, 2]
+ * <p>
+ * 输出: 2
+ * <p>
+ * 解释: 第三大的数不存在, 所以返回最大的数 2 .
+ * 示例 3:
+ * <p>
+ * 输入: [2, 2, 3, 1]
+ * <p>
+ * 输出: 1
+ * <p>
+ * 解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+ * 存在两个值为2的数，它们都排第二。
+ * 通过次数40,595提交次数114,185
+ *
+ * @author ezeal
+ */
+public class Simple414 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(thirdMax(nums));
+    }
+
+    public static int thirdMax(int[] nums) {
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
+        }
+        List<Integer> arrayList = new ArrayList<>(hashSet);
+        Collections.sort(arrayList);
+        if (arrayList.size() < 3) {
+            return arrayList.get(arrayList.size() - 1);
+        } else {
+            return arrayList.get(arrayList.size() - 3);
+        }
+    }
+}
