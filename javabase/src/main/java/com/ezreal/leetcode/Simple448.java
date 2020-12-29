4b02b32c1e78804ef2d9e43bd3d9eb89e0358ca7
黄佳明 <ezrealok@163.com>
2020-12-29 14:19:54 +0800

    feat: 448 commit

--- javabase/src/main/java/com/ezreal/leetcode/Simple448.java
+++ javabase/src/main/java/com/ezreal/leetcode/Simple448.java
@@ -25,26 +25,50 @@ import java.util.*;
  */
 public class Simple448 {
     public static void main(String[] args) {
-//        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
 //        int[] nums = {};
-        int[] nums = {2,2};
-        List<Integer> result = findDisappearedNumbers(nums);
+//        int[] nums = {2, 2};
+        List<Integer> result = findDisappearedNumbersWhitNoSpace(nums);
         System.out.println(result);
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        if (nums == null || nums.length == 0){
+        if (nums == null || nums.length == 0) {
             return new ArrayList<>();
         }
         int min = 1;
-        int max = min + nums.length-1;
+        int max = min + nums.length - 1;
         Set<Integer> hashSet = new HashSet<>();
-        for (int i = min;i<=max;i++){
+        for (int i = min; i <= max; i++) {
             hashSet.add(i);
         }
         for (int num : nums) {
             hashSet.remove(num);
         }
         return new ArrayList<>(hashSet);
+    }
+
+    public static List<Integer> findDisappearedNumbersWhitNoSpace(int[] nums) {
+        //用来存放结果
+        List<Integer> res = new ArrayList<>();
+        //1. 遍历下数组的元素，对对应的索引位置的元素作标记
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            //由于数组的元素有可能被*-1，所以取绝对值
+            int num = Math.abs(nums[i]);
+            int index = num - 1;
+            if (nums[index] > 0) {
+                nums[index] *= -1;
+            }
+        }
+        // 寻找没有标记的索引位置
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                //将索引转化为对应的元素
+                int num = i + 1;
+                res.add(num);
+            }
+        }
+        return res;
     }
 }
