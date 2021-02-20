0b71fd22d6f018499ccffce0596260544e466e52
黄佳明 <huangjiaming@souche.com>
2021-02-20 11:05:54 +0800

    feat: leetCode 刷题

--- javabase/src/main/java/com/ezreal/leetcode/array/Medium59.java
+++ javabase/src/main/java/com/ezreal/leetcode/array/Medium59.java
@@ -28,7 +28,8 @@ package com.ezreal.leetcode.array;
 public class Medium59 {
 
     public static void main(String[] args) {
-        int[][] ints = generateMatrix(5);
+//        int[][] ints = generateMatrix(5);
+        int[][] ints = generateMatrix2(5);
         for (int i = 0; i < ints.length; i++) {
             for (int j = 0; j < ints.length; j++) {
                 System.out.print("\t" + ints[i][j]);
@@ -81,5 +82,22 @@ public class Medium59 {
             ints[mid][mid] = count;
         }
         return ints;
+    }
+
+    public static int[][] generateMatrix2(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while (num <= tar) {
+            for (int i = l; i <= r; i++) mat[t][i] = num++; //left to right
+            t++;
+            for (int i = t; i <= b; i++) mat[i][r] = num++;// top to bottom
+            r--;
+            for (int i = r; i >= l; i--) mat[b][i] = num++;//right to left
+            b--;
+            for (int i=b;i>= t;i--) mat[i][l] = num++; // bottom to top
+            l++;
+        }
+        return mat;
     }
 }
