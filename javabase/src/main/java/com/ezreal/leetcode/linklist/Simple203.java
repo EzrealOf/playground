0ec5dc0a6c093361ef1e86e8681bc1b81e12cf45
ezrealok <ezrealok@163.com>
2021-05-18 20:43:12 +0800

    feat: add Simple203 demo

--- javabase/src/main/java/com/ezreal/leetcode/linklist/Simple203.java
+++ javabase/src/main/java/com/ezreal/leetcode/linklist/Simple203.java
@@ -7,6 +7,7 @@ package com.ezreal.leetcode.linklist;
  * <p>
  * 输入: 1->2->6->3->4->5->6, val = 6
  * 输出: 1->2->3->4->5
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0203.%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.md
  */
 public class Simple203 {
 
@@ -55,6 +56,63 @@ public class Simple203 {
         currentNode.next = node;
         return node;
     }
+
+    /**
+     * 添加虚节点方式
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElementsDemo1(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        // 因为删除可能涉及到头节点，所以设置dummy节点，统一操作
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+    /**
+     * 不添加虚拟节点方式
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElementsDemo2(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        // 已经为null，提前退出
+        if (head == null) {
+            return head;
+        }
+        // 已确定当前head.val != val
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
 
 
 }
